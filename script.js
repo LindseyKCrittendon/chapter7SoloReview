9158957a74795d265954ae88d787c0a3bcb097b1
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-03-26 18:49:49 -0400

    trucking along

--- script.js
+++ script.js
@@ -120,6 +120,69 @@ console.log(familyArray[1].hobbies[2]);
 console.log(`${familyArray[1].firstName} tries to get out of her homework all the time by ${familyArray[1].hobbies[2]}!`) //omfg it feels good to understand that.
 
 
+// Lightning Exercise
+// All About Me Pt. 2
+// Instead of storing the data from your first lightning exercise in separate variables, store them in an object. Your object should have a property of firstName, lastName, age, likesToTravel. It should also have a property called favoriteMovies, which should hold the array of your favorite movies you created in the last lightning exercise.
+
+var lindseyInfo = {
+    firstName: "Lindsey",
+    lastName: "Crittendon",
+    age: 34,
+    likesToTravel: true,
+    favoriteMovies: ['pulp fiction', 'eternal sunshine of the spotless mind']
+}
+console.log(lindseyInfo);
+console.log(lindseyInfo.favoriteMovies[1]) //it's all so clear. holy tittyfarts, batman
+
+// 1. Today's weather
+// In your JavaScript file, instantiate and assign the following variables with information about the current weather:
+// temperature should hold a number representing the current temperature in Fahrenheit
+// conditions should hold a string about the current weather conditions (i.e. "cloudy", "sunny", etc).
+// sunriseTime should hold a string representing what time the sun rose this morning
+// sunsetTime should hold a string representing what time the sun will set this evening
+// Log each variable to the console.
+// Use string template literals to combine these variables into a sentence about the weather. (Example: "It's currently sunny and 55 degrees. The sun rose this morning at 7:34 AM and will set at 5:09 PM.")
+
+var todaysWeather = {
+    temperature: 73,
+    conditions: "bright and sunny",
+    sunriseTime: "7:03AM",
+    sunsetTime: "8:15PM",
+    };
+    console.log(todaysWeather);
+    console.log(todaysWeather.temperature);
+    console.log(todaysWeather.conditions);
+    console.log(todaysWeather.sunriseTime);
+    console.log(todaysWeather.sunsetTime);
+    console.log(`It's currently ${todaysWeather.conditions} and ${todaysWeather.temperature}.  The sun rose this morning at ${todaysWeather.sunriseTime} and will set at ${todaysWeather.sunsetTime}`);
+
+//     2. Weather Data
+// In your JavaScript file, copy and paste the following array:
+
+// var highTemperatures = [55, 57, 53, 47, 56, 50];
+// var lowTemperatures = [33, 30, 29, 35, 31, 32]; 
+// Print the first high temperature to the console.
+// Print the last low temperature to the console.
+// What is the average high temperature from the data set?
+// What is the average low temperature from the data set?
+var highTemperatures = [55, 57, 53, 47, 56, 50];
+var lowTemperatures = [33, 30, 29, 35, 31, 32];
+console.log(highTemperatures[0]);
+console.log(lowTemperatures[5]);
+
+var highTemperatureSum = 
+
+
+
+
+    
+
+
+
+
+
+
+
 
 
 
